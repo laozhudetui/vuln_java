3031d9198017fc6150dccbbdfab746bd124c07fa
MyKings <xsseroot@gmail.com>
2019-07-22 11:07:44 +0800

    Initialization project

---
+++ src/main/java/com/seecode/auth/Authenticate.java
@@ -0,0 +1,18 @@
+package com.seecode.auth;
+
+/**
+ * Created by MyKings on 19/07/2019.
+ */
+public class Authenticate {
+
+    public void LoginPasswordLeak(){
+        // password leak
+        String password = "1qaz@WSX";
+    }
+
+
+    public  void LoginFalsePositive(){
+
+        String password = "pwd_txt";
+    }
+}
